90ef16a8275e9192c75d0c2108d5808549aa1282
DELL <DELL@SravyaPC2>
2025-07-22 15:38:44 +0530

    The Constructors

---
+++ C2TC_CoreJavaProgram/src/Day3/Customer.java
@@ -0,0 +1,47 @@
+package Day3;
+
+public class Customer {
+
+	
+		private int cid;
+		private String name;
+		private String city;
+		
+		//explicit constructor
+		public Customer()
+		{
+			this.cid=101;
+			this.name="retz";
+			this.city="pune";
+		}
+		public Customer(int cid, String name, String city)
+		{
+			this.cid=cid;
+			this.name=name;
+			this.city=city;
+		}
+		public int getCid() {
+			return cid;
+		}
+		public void setCid(int cid) {
+			this.cid = cid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		@Override
+		public String toString() {
+			return "Customer [cid=" + cid + ", name=" + name + ", city=" + city + "]";
+		}
+		
+
+}
---
+++ C2TC_CoreJavaProgram/src/Day3/CustomerDemo.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c1 = new Customer();
+		System.out.println(c1);
+		Customer c2 = new Customer(102,"harshi","ongole");
+		System.out.println(c2);
+	}
+
+}
