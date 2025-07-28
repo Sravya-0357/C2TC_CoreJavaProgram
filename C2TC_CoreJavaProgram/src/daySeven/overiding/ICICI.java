6a0c2ca4c6913e309dc2f8a2fba3738e882e8c49
DELL <DELL@SravyaPC2>
2025-07-28 19:39:55 +0530

    Day 7 work

---
+++ C2TC_CoreJavaProgram/src/daySeven/overiding/ICICI.java
@@ -0,0 +1,11 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package daySeven.overiding;
+
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/daySeven/overiding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package daySeven.overiding;
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
