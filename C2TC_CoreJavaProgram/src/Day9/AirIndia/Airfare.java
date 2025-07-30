31251a3c6b2f93fd7239a17aa969ed6eec487468
DELL <DELL@SravyaPC2>
2025-07-30 21:53:45 +0530

    Day 9 Tasks

--- C2TC_CoreJavaProgram/src/Day9/AirIndia/Airfare.java
+++
@@ -1,5 +0,0 @@
-package Day9.AirIndia;
-
-public interface Airfare {
-    double calculateAmount();
-}
---
+++ C2TC_CoreJavaProgram/src/dayNine/StringSubclass.java
@@ -0,0 +1,10 @@
+//can't inherit final classes
+package dayNine;
+
+public class StringSubclass extends StringOperationsDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
